57819bb5eec3e044272b7d321e1da297f1a35ce4
jandi <acidlime1987@naver.com>
2023-05-12 15:47:17 +0900

    update

---
+++ mainvisual.php
@@ -0,0 +1,11 @@
+<section class="mainvisual">
+    <figure class="bg">
+        <img src="./css/images/main_bg_1.jpg" alt="배경이미지1">
+        <div></div>
+    </figure>
+    <div class="mainvisual-wrap">
+        <div class="mainpic">
+            
+        </div>
+    </div>
+</section>
