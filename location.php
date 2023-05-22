fc1bafe1d8159ef4f1dc1b25b03aba0cd9d3d0af
jandi <acidlime1987@naver.com>
2023-05-22 17:29:51 +0900

    calendar 완료

---
+++ location.php
@@ -0,0 +1,41 @@
+<div class="location">
+    <div class="loca_title">
+        <h3>오시는 길</h3>
+        <p class="loca_name">예술의 전당 웨딩홀</p>
+        <p class="loca_address">서울특별시 서초구 서초3동 남부순환로 2406</p>
+    </div>
+    <div class="map_wrap">
+    <div id="daumRoughmapContainer1684742894173" class="root_daum_roughmap root_daum_roughmap_landing"></div>
+    </div>
+
+    <ul class="loca_info">
+        <li>
+            <p>지하철</p>
+            <p>3호선 남부터미널역 2번 출구 도보 10분</p>
+        </li>
+        <li>
+            <p>버스</p>
+            <p>예술의전당 역 하차 도보 10분</p>
+        </li>
+        <li>
+            <p>셔틀버스</p>
+            <p>3호선 남부터미널역 3번 출구에서 15분 간격으로 출발합니다.</p>
+        </li>
+    </ul>
+
+    <div class="btn-wrap">
+        <button>카카오내비</button>
+        <button>웨딩홀에 전화</button>
+    </div>
+</div>
+
+
+<script charset="UTF-8" class="daum_roughmap_loader_script" src="https://ssl.daumcdn.net/dmaps/map_js_init/roughmapLoader.js"></script>
+
+
+<script charset="UTF-8">
+	new daum.roughmap.Lander({
+		"timestamp" : "1684742894173",
+		"key" : "2evep",
+	}).render();
+</script>
