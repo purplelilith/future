65835c8df88a9c6e2392850cac021eaf64a15f56
jandi <acidlime1987@naver.com>
2023-05-22 17:48:12 +0900

    업데이트

--- account.php
+++ account.php
@@ -1,3 +1,44 @@
 <div class="account">
-    
+    <h3>마음 전하실 곳</h3>
+
+    <ul>
+        <li>
+            <a href="">신랑 측에게 마음 전하기</a>
+            <ul>
+                <li>
+                    <p>
+                        신한은행 123-45-56789
+                        <span>정민현</span>
+                    </p>
+                    <button>계좌 복사하기</button>
+                </li>
+                <li>
+                    <p>
+                        국민은행 123-45-56789
+                        <span>정진한</span>
+                    </p>
+                    <button>계좌 복사하기</button>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="">신부 측에게 마음 전하기</a>
+            <ul>
+                <li>
+                    <p>
+                        신한은행 123-45-56789
+                        <span>이주연</span>
+                    </p>
+                    <button>계좌 복사하기</button>
+                </li>
+                <li>
+                    <p>
+                        국민은행 123-45-56789
+                        <span>이형규</span>
+                    </p>
+                    <button>계좌 복사하기</button>
+                </li>
+            </ul>
+        </li>
+    </ul>
 </div>
