98ca152bc7816a791699a6f3db41cf5a53ffdb80
jandi <acidlime1987@naver.com>
2023-05-23 17:21:51 +0900

    nav 업데이트

--- nav.php
+++ nav.php
@@ -1,6 +1,17 @@
 <nav>
     <ul>
-        <li><a href=""><i class="fa-solid fa-phone-volume"></i></a></li>
+        <li>
+            <a href=""><i class="fa-solid fa-phone-volume"></i></a>
+            <ul class="second-depth">
+                <li><a href="tel:010-111-1111"><span class="groom">신랑</span> 정민현 님께 연락하기</a></li>
+                <li><a href="tel:010-111-1111"><span class="groom">신랑측 父</span> 정진한 님</a></li>
+                <li><a href="tel:010-111-1111"><span class="groom">신랑측 母</span> 한수진 님</a></li>
+                <li><a href="tel:010-111-1111"><span class="bride">신부</span> 이주연 님께 연락하기</a></li>
+                <li><a href="tel:010-111-1111"><span class="bride">신부측 父</span> 이형규 님</a></li>
+                <li><a href="tel:010-111-1111"><span class="bride">신부측 母</span> 박영은 님</a></li>
+                <li><a href="tel:010-111-1111" class="cancel">취소</a></li>
+            </ul>
+        </li>
         <li><a href=""><i class="fa-solid fa-camera"></i></a></li>
         <li><a href=""><i class="fa-solid fa-calendar-days"></i></a></li>
         <li><a href=""><i class="fa-solid fa-location-dot"></i></a></li>
@@ -8,3 +19,7 @@
         <li><a href=""><i class="fa-solid fa-share-nodes"></i></a></li>
     </ul>
 </nav>
+
+<script>
+    
+</script>
